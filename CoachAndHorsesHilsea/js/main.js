4a55f2b72b252ef10f98f6f54647751318b873ef
David Otton <david@otton.org>
2018-08-15 12:29:04 +0100

    code format, remove some common code from listeners

--- CoachAndHorsesHilsea/js/main.js
+++ CoachAndHorsesHilsea/js/main.js
@@ -1,75 +1,77 @@
 (function(){
     // Vertical Timeline - by CodyHouse.co
-	function VerticalTimeline( element ) {
-		this.element = element;
-		this.blocks = this.element.getElementsByClassName("js-cd-block");
-		this.images = this.element.getElementsByClassName("js-cd-img");
-		this.contents = this.element.getElementsByClassName("js-cd-content");
-		this.offset = 0.8;
-		this.hideBlocks();
-	};
+    function VerticalTimeline( element ) {
+        this.element = element;
+        this.blocks = this.element.getElementsByClassName("js-cd-block");
+        this.images = this.element.getElementsByClassName("js-cd-img");
+        this.contents = this.element.getElementsByClassName("js-cd-content");
+        this.offset = 0.8;
+        this.hideBlocks();
+    };
 
-	VerticalTimeline.prototype.hideBlocks = function() {
-		//hide timeline blocks which are outside the viewport
-		if ( !"classList" in document.documentElement ) {
-			return;
-		}
-		var self = this;
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
-					self.images[i].classList.add("cd-is-hidden"); 
-					self.contents[i].classList.add("cd-is-hidden"); 
-				}
-			})(i);
-		}
-	};
+    VerticalTimeline.prototype.hideBlocks = function() {
+        //hide timeline blocks which are outside the viewport
+        if ( !"classList" in document.documentElement ) {
+            return;
+        }
+        var self = this;
+        for( var i = 0; i < this.blocks.length; i++) {
+            (function(i){
+                if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
+                    self.images[i].classList.add("cd-is-hidden");
+                    self.contents[i].classList.add("cd-is-hidden");
+                }
+            })(i);
+        }
+    };
 
-	VerticalTimeline.prototype.showBlocks = function() {
-		if ( ! "classList" in document.documentElement ) {
-			return;
-		}
-		var self = this;
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.contents[i].classList.contains("cd-is-hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
-					// add bounce-in animation
-					self.images[i].classList.add("cd-timeline__img--bounce-in");
-					self.contents[i].classList.add("cd-timeline__content--bounce-in");
-					self.images[i].classList.remove("cd-is-hidden");
-					self.contents[i].classList.remove("cd-is-hidden");
-				}
-			})(i);
-		}
-	};
+    VerticalTimeline.prototype.showBlocks = function() {
+        if ( ! "classList" in document.documentElement ) {
+            return;
+        }
+        var self = this;
+        for( var i = 0; i < this.blocks.length; i++) {
+            (function(i){
+                if( self.contents[i].classList.contains("cd-is-hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
+                    // add bounce-in animation
+                    self.images[i].classList.add("cd-timeline__img--bounce-in");
+                    self.contents[i].classList.add("cd-timeline__content--bounce-in");
+                    self.images[i].classList.remove("cd-is-hidden");
+                    self.contents[i].classList.remove("cd-is-hidden");
+                }
+            })(i);
+        }
+    };
 
-	var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
-		verticalTimelinesArray = [],
-		scrolling = false;
-	if( verticalTimelines.length > 0 ) {
-		for( var i = 0; i < verticalTimelines.length; i++) {
-			(function(i){
-				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
-			})(i);
-		}
+    var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
+        verticalTimelinesArray = [],
+        scrolling = false;
+    if( verticalTimelines.length > 0 ) {
+        for( var i = 0; i < verticalTimelines.length; i++) {
+            (function(i){
+                verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
+            })(i);
+        }
 
-		//show timeline blocks on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
-			}
-		});
-	}
+        //show timeline blocks on scrolling
+        window.addEventListener("scroll", function(event) {
+            if( !scrolling ) {
+                scrolling = true;
+                (!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
+            }
+        });
+    }
 
-	function checkTimelineScroll() {
-		verticalTimelinesArray.forEach(function(timeline){
-			timeline.showBlocks();
-		});
-		scrolling = false;
-	};
+    function checkTimelineScroll() {
+        verticalTimelinesArray.forEach(function(timeline){
+            timeline.showBlocks();
+        });
+        scrolling = false;
+    };
 
-	// Flip display:none on an element on or off.
+    // Functions below this point should be genericized, so we can have more than one timeline per page.
+
+    // Flip display:none on an element on or off.
     function toggleClass(element, className) {
         if (element.classList.contains(className)) {
             element.classList.remove(className);
@@ -86,49 +88,38 @@
         }
     }
 
+    // Whatever str this's innerText currently is, flip to the other str.
+    function toggleInnerText(element, str1, str2) {
+        if (element.innerText == str1) {
+            element.innerText = str2;
+        } else {
+            element.innerText = str1;
+        }
+    }
+
     document.getElementById('toggleNewspapers').addEventListener('click', function() {
         toggleAllDisplayNone('cd-timeline__img--newspaper');
-        if (this.innerText == 'Hide Newspapers') {
-            this.innerText = "Show Newspapers";
-        } else {
-            this.innerText = "Hide Newspapers";
-        }
+        toggleInnerText(this, 'Hide Newspapers', 'Show Newspapers');
         checkTimelineScroll();
     });
     document.getElementById('toggleBooks').addEventListener('click', function() {
         toggleAllDisplayNone('cd-timeline__img--book');
-        if (this.innerText == 'Hide Books') {
-            this.innerText = "Show Books";
-        } else {
-            this.innerText = "Hide Books";
-        }
+        toggleInnerText(this, 'Hide Books', 'Show Books');
         checkTimelineScroll();
     });
     document.getElementById('toggleComments').addEventListener('click', function() {
         toggleAllDisplayNone('cd-timeline__img--comment');
-        if (this.innerText == 'Hide Comments') {
-            this.innerText = "Show Comments";
-        } else {
-            this.innerText = "Hide Comments";
-        }
+        toggleInnerText(this, 'Hide Comments', 'Show Comments');
         checkTimelineScroll();
     });
     document.getElementById('toggleMaps').addEventListener('click', function() {
         toggleAllDisplayNone('cd-timeline__img--location');
-        if (this.innerText == 'Hide Maps') {
-            this.innerText = "Show Maps";
-        } else {
-            this.innerText = "Hide Maps";
-        }
+        toggleInnerText(this, 'Hide Maps', 'Show Maps');
         checkTimelineScroll();
     });
     document.getElementById('toggleCensuses').addEventListener('click', function() {
         toggleAllDisplayNone('cd-timeline__img--census');
-        if (this.innerText == 'Hide Censuses') {
-            this.innerText = "Show Censuses";
-        } else {
-            this.innerText = "Hide Censuses";
-        }
+        toggleInnerText(this, 'Hide Censuses', 'Show Censuses');
         checkTimelineScroll();
     });
 
